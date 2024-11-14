b38ec496fc53f30716ec62e99fb1916c6b96b972
Kyushokugorira <122949364+Kyushokugorira@users.noreply.github.com>
2024-11-14 14:29:56 +0900

    Initial gh-pages commit

---
+++ src/index.ts
@@ -0,0 +1,2 @@
+console.log("実行");
+import "./prac10";
---
+++ src/prac01.ts
@@ -0,0 +1,13 @@
+export {};
+import dayjs from "dayjs";
+import 'dayjs/locale/ja';
+
+dayjs.locale('ja');
+const dtFmt = "YYYY/MM/DD(dd) HH:mm";
+const deadline: Date = new Date(2024, 10, 2, 11, 45);
+const cratedAt: Date = new Date(); // 引数なしで現在日時を取得
+
+const str =
+  `期限 ${dayjs(deadline).format(dtFmt)}` +
+  `(登録日 ${dayjs(cratedAt).format(dtFmt)})`;
+console.log(str);
